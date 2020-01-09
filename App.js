ee4f8a51a2b499c6da46f069318f7271284b0fdc
Glenn Fernandes <glennferns93@gmail.com>
2020-01-09 19:08:03 +0530

    init commit + Added Paper Theme + Added react-navigation + Bottom Bar Navigation + Theme color draft selection + Main Pages

--- App.js
+++ App.js
@@ -1,38 +1,161 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Icon from "react-native-vector-icons/Ionicons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
+class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Details!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {/* other code from before here */}
+        <Text>All events and Users</Text>
+      </View>
+    );
+  }
+}
+
+class FavoritesScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {/* other code from before here */}
+        <Text>Favorites</Text>
+      </View>
+    );
+  }
+}
+
+class AddEventScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {/* other code from before here */}
+        <Text>Add</Text>
+      </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {/* other code from before here */}
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
+      </View>
+    );
+  }
+}
+
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: `Train Time`,
+      headerTitleStyle: { color: "#FFF" },
+      headerStyle: { backgroundColor: "#282c2f" },
+      headerRightContainerStyle: {
+        marginRight: 20,
+      },
+      headerRight:()=>(
+        <Icon
+              name="md-search"
+              size={20}
+              color={"#2699FB"}
+              onPress={() => navigation.navigate("Details")}
+        />
+      ),
+    })
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  Details: { screen: DetailsScreen }
 });
+
+const SettingsStack = createStackNavigator({
+  Settings: { screen: SettingsScreen },
+  Details: { screen: DetailsScreen }
+});
+
+const FavoritesStack = createStackNavigator({
+  Settings: { screen: FavoritesScreen },
+  Details: { screen: DetailsScreen }
+});
+
+const AddEventStack = createStackNavigator({
+  Settings: { screen: AddEventScreen },
+  Details: { screen: DetailsScreen }
+});
+
+export default createAppContainer(
+  createMaterialBottomTabNavigator(
+    {
+      Home: {
+        screen: HomeStack,
+        navigationOptions: {
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="md-pulse"
+              size={20}
+              color={focused ? "#2699FB" : "#848a8e"}
+            />
+          )
+        }
+      },
+      Favorites: {
+        screen: FavoritesStack,
+        navigationOptions: {
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="ios-heart"
+              size={20}
+              color={focused ? "#2699FB" : "#848a8e"}
+            />
+          )
+        }
+      },
+      Create: {
+        screen: AddEventStack,
+        navigationOptions: {
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="ios-add"
+              size={25}
+              color={focused ? "#2699FB" : "#848a8e"}
+            />
+          )
+        }
+      },
+      Settings: {
+        screen: SettingsStack,
+        navigationOptions: {
+          tabBarIcon: ({ focused }) => (
+            <Icon
+              name="md-person"
+              size={20}
+              color={focused ? "#2699FB" : "#848a8e"}
+            />
+          )
+        }
+      }
+    },
+    {
+      initialRouteName: "Home",
+      activeColor: "#2699FB",
+      barStyle: { backgroundColor: "#282c2f" }
+    }
+  )
+);
