7bdc94ab9523c6666cdf2e2acbbb360f3cef28ff
Aaron Saldanha <ajay@octalogic.in>
2020-01-13 16:37:58 +0530

    create event

--- src/components/CreateScreen/index.js
+++ src/components/CreateScreen/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, Picker } from "react-native";
 import { TextInput } from "react-native-paper";
 import { Dropdown } from "react-native-material-dropdown";
+import { Button } from 'react-native-paper';
 
 const CreateScreen = () => {
   const [name, setName] = useState("");
@@ -21,9 +22,10 @@ const CreateScreen = () => {
         containerStyle={{
           borderWidth: 1,
           borderRadius:2,
-          borderColor: "gray",
+          borderColor: "grey",
           borderStyle: "solid",
-          padding: 0,
+          paddingRight: 6,
+          paddingLeft: 6,
           marginVertical:10,
         }}
         pickerStyle={{
@@ -50,6 +52,130 @@ const CreateScreen = () => {
           }
         ]}
       />
+      <Dropdown
+        label="Language"
+        dropdownMargins={{ min: 5, max: 10 }}
+        containerStyle={{
+          borderWidth: 1,
+          borderRadius:2,
+          borderColor: "grey",
+          borderStyle: "solid",
+          paddingRight: 6,
+          paddingLeft: 6,
+          marginVertical:10,
+        }}
+        pickerStyle={{
+          marginVertical:0,
+        }}
+        data={[
+          {
+            value: "English"
+          },
+          {
+            value: "Portuguese"
+          },
+          {
+            value: "Spanish"
+          }
+        ]}
+      />
+      <Dropdown
+        label="Country"
+        dropdownMargins={{ min: 5, max: 10 }}
+        containerStyle={{
+          borderWidth: 1,
+          borderRadius:2,
+          borderColor: "grey",
+          borderStyle: "solid",
+          paddingRight: 6,
+          paddingLeft: 6,
+          marginVertical:10,
+        }}
+        pickerStyle={{
+          marginVertical:0,
+        }}
+        data={[
+          {
+            value: "India"
+          },
+          {
+            value: "Portugal"
+          },
+          {
+            value: "Spain"
+          },
+          {
+            value: "Pakistan"
+          },
+      
+        ]}
+      />
+      <Dropdown
+        label="City"
+        dropdownMargins={{ min: 5, max: 10 }}
+        containerStyle={{
+          borderWidth: 1,
+          borderRadius:2,
+          borderColor: "grey",
+          borderStyle: "solid",
+          paddingRight: 6,
+          paddingLeft: 6,
+          marginVertical:10,
+        }}
+        pickerStyle={{
+          marginVertical:0,
+        }}
+        data={[
+          {
+            value: "Melbourne"
+          },
+          {
+            value: "London"
+          },
+          {
+            value: "Sydney"
+          },
+          {
+            value: "Paris"
+          },
+          {
+            value: "New York"
+          },
+          {
+            value: "Milan"
+          }
+        ]}
+      />
+      <Dropdown
+        label="Number of Participants"
+        dropdownMargins={{ min: 5, max: 10 }}
+        containerStyle={{
+          borderWidth: 1,
+          borderRadius:2,
+          borderColor: "grey",
+          borderStyle: "solid",
+          paddingRight: 6,
+          paddingLeft: 6,
+          marginVertical:10,
+        }}
+        pickerStyle={{
+          marginVertical:0,
+        }}
+        data={[
+          {
+            value: "1"
+          },
+          {
+            value: "2"
+          },
+          {
+            value: "3"
+          },
+        ]}
+      />
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+    Create Event
+  </Button>
     </View>
   );
 };
