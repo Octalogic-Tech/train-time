12812fa7871de01e286093c2e36fefea749f3886
Glenn Fernandes <glennferns93@gmail.com>
2020-01-13 10:58:41 +0530

    name and dropdown

--- src/components/CreateScreen/index.js
+++ src/components/CreateScreen/index.js
@@ -1,14 +1,57 @@
-import React from "react";
-import { View, Text } from "react-native";
-
+import React, { useState } from "react";
+import { View, Text, Picker } from "react-native";
+import { TextInput } from "react-native-paper";
+import { Dropdown } from "react-native-material-dropdown";
 
 const CreateScreen = () => {
+  const [name, setName] = useState("");
+  const [sport, setSport] = useState("");
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-		<Text>Create Form</Text>
-	</View>
+    <View style={{ margin: 10 }}>
+      <TextInput
+        label="Name"
+        value={name}
+        mode="outlined"
+        onChangeText={text => setName({ text })}
+      />
+      <Dropdown
+        label="Sport"
+        dropdownMargins={{ min: 5, max: 10 }}
+        containerStyle={{
+          borderWidth: 1,
+          borderRadius:2,
+          borderColor: "gray",
+          borderStyle: "solid",
+          padding: 0,
+          marginVertical:10,
+        }}
+        pickerStyle={{
+          marginVertical:0,
+        }}
+        data={[
+          {
+            value: "Archery"
+          },
+          {
+            value: "Rowing"
+          },
+          {
+            value: "Bowling"
+          },
+          {
+            value: "Croquet"
+          },
+          {
+            value: "Jogging"
+          },
+          {
+            value: "Badminton"
+          }
+        ]}
+      />
+    </View>
   );
 };
 
-
-export default CreateScreen
+export default CreateScreen;
